1decb096905ecf7ec1368dac04aaf2f98a1b27e4
Maksim Titov <mstitov.mail@gmail.com>
2025-09-21 17:22:16 +0300

    feat(articleParamsForm) – Сайдбар с формой изменения стилей.

--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,55 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
+import { StrictMode, CSSProperties, useState } from 'react';
 import clsx from 'clsx';
-
 import { Article } from './components/article/Article';
 import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './constants/articleProps';
-
+import {
+	ArticleStateType,
+	defaultArticleState,
+} from './constants/articleProps';
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
 
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
+export type StyleProps = {
+	fontFamily: string;
+	fontSize: string;
+	fontColor: string;
+	containerWidth: string;
+	bgColor: string;
+};
+
 const App = () => {
+	const [styleState, setStyleState] =
+		useState<ArticleStateType>(defaultArticleState);
+
+	const updateStyles = (updates: Partial<ArticleStateType>) => {
+		setStyleState((prev) => ({ ...prev, ...updates }));
+	};
+
+	const resetStyles = () => {
+		setStyleState(defaultArticleState);
+	};
+
 	return (
 		<main
 			className={clsx(styles.main)}
 			style={
 				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
+					'--font-family': styleState.fontFamilyOption.value,
+					'--font-size': styleState.fontSizeOption.value,
+					'--font-color': styleState.fontColor.value,
+					'--container-width': styleState.contentWidth.value,
+					'--bg-color': styleState.backgroundColor.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm />
+			<ArticleParamsForm
+				initialValues={defaultArticleState}
+				onUpdate={updateStyles}
+				onReset={resetStyles}
+			/>
 			<Article />
 		</main>
 	);
